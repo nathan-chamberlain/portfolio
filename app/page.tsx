5426eb665fbe74df493a47790f1e7ff0dd61f353
chambenath <chambenath@ecs.vuw.ac.nz>
2025-07-09 19:37:40 +1200

    Update portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -37,11 +37,13 @@ const Home = () => {
 
 
 export default function HomePage() {
-  <Suspense fallback={
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-    </div>
-  }>
-    <Home />
-  </Suspense>
+  return (
+    <Suspense fallback={
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    }>
+      <Home />
+    </Suspense>
+  );
 }
